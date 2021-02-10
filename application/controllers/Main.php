b2684a1ac067e45fb99f679e5ca60812dfda3603
Call Vin <kanggara75@gmail.com>
2021-02-10 13:38:09 +0700

    using CI3

---
+++ application/controllers/Main.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Main extends CI_Controller
+{
+  public function index()
+  {
+    $this->load->view('landing/');
+  }
+}
+
+class Join extends CI_Controller
+{
+  public function index()
+  {
+    $this->load->view('landing/');
+  }
+}
+
+class Buat extends CI_Controller
+{
+
+  public function index()
+  {
+    $this->load->view('landing/');
+  }
+}
+
+class VsCom extends CI_Controller
+{
+  public function index()
+  {
+    $this->load->view('landing/');
+  }
+}
